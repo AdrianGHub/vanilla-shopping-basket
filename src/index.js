fdd18d096d0ae1c486d282c967f4ab5599dce16b
Adrian <adrian_zinko@op.pl>
2020-04-23 07:30:23 +0200

    feat: query dom elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import '../sass/style.scss';
 
-console.log('Boilerplate is working!');
+console.log('Let\'s buy something! 🤭🛒');
+
+const purchaseButton = document.querySelector('.buy--js');
+const removeProductButtons = document.querySelectorAll('.remove--js');
+const alertPopup = document.querySelector('.empty--js');
+let productsQuantity = document.querySelectorAll('.items--js');
+let totalPrice = document.querySelector('.price--js');
