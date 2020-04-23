7e831d6ece3cd482a37ea108b7a4c7d6efeea544
Adrian <adrian_zinko@op.pl>
2020-04-23 19:20:43 +0200

    feat: display alerts / activate purchase button

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import '../sass/style.scss';
 
 console.log('Let\'s buy something! 🤭🛒');
 
-const purchaseButton = document.querySelector('.buy--js');
+const purchaseButton = document.querySelector('.purchase--js');
 const removeProductButtons = document.querySelectorAll('.remove--js');
-const alertPopup = document.querySelector('.empty--js');
+const alertPopup = document.querySelector('.popup__empty--js');
 let productsQuantity = document.querySelectorAll('.items--js');
 let totalPrice = document.querySelector('.price--js');
+
+checkBasketAlerts('empty')
 
 
 function removeProduct(e) {
     e.target.closest('.product--js').remove();
     productsQuantity = document.querySelectorAll('.items--js');
     updatePrice();
+    checkBasketAlerts('empty')
 }
 
 
@@ -38,4 +41,38 @@ productsQuantity.forEach(amount => {
     amount.addEventListener('change', updatePrice)
   })
 
-// purchaseButton.addEventListener('click');
+function checkBasketAlerts(alertFlag) {
+    switch (alertFlag) {
+
+        case 'empty': 
+            alertPopup.classList.remove('bg-teal-100', 'border-teal-500', 'text-teal-900');
+            alertPopup.classList.add('bg-orange-100', 'border-range-500', 'text-orange-700');
+            alertPopup.firstElementChild.textContent = 'Uwaga';
+            alertPopup.lastElementChild.textContent = 'Twój koszyk jest pusty.';
+            if(productsQuantity.length === 0) {
+                alertPopup.style.display = 'block';
+                purchaseButton.style.display = 'none';
+            } else {
+                alertPopup.style.display = 'none';
+            }
+            break;
+
+        case 'success':
+            alertPopup.classList.remove('bg-orange-100', 'border-orange-500', 'text-orange-700');
+            alertPopup.classList.add('bg-teal-100', 'border-teal-500', 'text-teal-900');
+            alertPopup.firstElementChild.textContent = 'Gratulację!';
+            alertPopup.lastElementChild.textContent = 'Dokonałeś właściwego wyboru!';
+            productsQuantity.length !== 0
+            ? alertPopup.style.display = 'block' : null;
+            setInterval(() => {
+                alertPopup.style.display = 'none';
+                document.querySelectorAll('.product--js').forEach(product => product.innerHTML = '');
+                purchaseButton.style.display = 'none';
+            }, 3000);
+            setTimeout(() => {
+                document.querySelector('.text-2xl').innerHTML = '<p style="text-align: center;">Dziękujemy za zakupy. Zapraszamy ponownie.</p>';
+            }, 3000)
+    }
+}
+
+purchaseButton.addEventListener('click', () => checkBasketAlerts('success'));
